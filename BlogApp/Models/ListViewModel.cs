e505aeadf408003e975b94b3851733d3f302176a
SalcBrau <braulioosalcedo@gmail.com>
2019-09-09 22:35:07 -0500

    Increased second ListViewModel.cs constructor flexibility
    * Now works for retrieving both 'Category' and 'Tag' posts

--- BlogApp/Models/ListViewModel.cs
+++ BlogApp/Models/ListViewModel.cs
@@ -16,16 +16,27 @@ namespace BlogApp.Models
             TotalPosts = repositoryWrapper.Post.TotalPosts();
         }
 
-        public ListViewModel(IRepositoryWrapper repositoryWrapper, string categorySlug, int p)
+        public ListViewModel(IRepositoryWrapper repositoryWrapper, string text, string type, int p)
         {
-            Posts = repositoryWrapper.Post.PostsForCategory(categorySlug, p - 1, 10);
-            TotalPosts = repositoryWrapper.Post.TotalPostsForCategory(categorySlug);
-            Category = repositoryWrapper.Category.Category(categorySlug);
+            switch (type)
+            {
+                case "Category":
+                    Posts = repositoryWrapper.Post.PostsForCategory(text, p - 1, 10);
+                    TotalPosts = repositoryWrapper.Post.TotalPostsForCategory(text);
+                    Category = repositoryWrapper.Category.Category(text);
+                    break;
+                default:
+                    Posts = repositoryWrapper.Post.PostsForTag(text, p - 1, 10);
+                    TotalPosts = repositoryWrapper.Post.TotalPostsForTag(text);
+                    Tag = repositoryWrapper.Tag.Tag(text);
+                    break;
+            }
         }
 
         public ICollection<Post> Posts { get; private set; }
         public int TotalPosts { get; private set; }
         public Category Category { get; private set;}
+        public Tag Tag { get; private set; }
 
     }
 }
