bf98f3f55b75387728a1a0384015cc466374665b
SalcBrau <braulioosalcedo@gmail.com>
2019-09-13 17:09:50 -0500

    Created WidgetViewModel.cs

---
+++ BlogApp/Models/WidgetViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Data.Entities;
+using Data.Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Models
+{
+    public class WidgetViewModel
+    {
+        public WidgetViewModel(IRepositoryWrapper repositoryWrapper)
+        {
+            Categories = repositoryWrapper.Category.Categories();
+        }
+
+        public ICollection<Category> Categories { get; private set; }
+    }
+}
