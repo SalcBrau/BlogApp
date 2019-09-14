a99afe73399bd43387ed98b22358575dee5ab418
SalcBrau <braulioosalcedo@gmail.com>
2019-09-14 11:33:51 -0500

    Created Sidebars and Tags view components
    * Sidebars view component creates the widget
    * Tags view component renders tags for posts in listings

---
+++ BlogApp/ViewComponents/SidebarsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Models;
+using Data.Repo.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.ViewComponents
+{
+    public class SidebarsViewComponent : ViewComponent
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+
+        public SidebarsViewComponent(IRepositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var widgetViewModel = new WidgetViewModel(_repositoryWrapper);
+            return View("_Sidebars", widgetViewModel);
+        }
+    }
+}
---
+++ BlogApp/ViewComponents/TagsViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.ViewComponents
+{
+    public class TagsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Post model)
+        {
+            return View("_Tags", model);
+        }
+        
+    }
+}
