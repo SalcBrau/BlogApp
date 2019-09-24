cdee61d871fe33e2133bb5e45daa6335f26c352f
SalcBrau <braulioosalcedo@gmail.com>
2019-09-24 14:54:35 -0500

    Created admin controller with get and post login action

---
+++ BlogApp/Controllers/AdminController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BlogApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogApp.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AdminController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet, AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    return Redirect(returnUrl);
+
+                return RedirectToAction("Manage");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost, AllowAnonymous, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _configuration.GetSection("SiteUser").Get<SiteUser>();
+                var passwordHasher = new PasswordHasher<string>();
+                if (model.UserName == user.UserName && passwordHasher.VerifyHashedPassword(null, user.Password, model.Password) == PasswordVerificationResult.Success)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, model.UserName)
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var authProperties = new AuthenticationProperties();
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+                    
+                    if (!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                        return Redirect(returnUrl);
+
+                    return RedirectToAction("Manage");
+                }
+                ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            }
+        
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        public async Task<ActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Login", "Admin");
+        }
+    }
+}
